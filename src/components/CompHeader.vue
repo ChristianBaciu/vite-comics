82242f0574442617a58e2bf167dda9d658d27b23
ChristianBaciu <christian3110@outlook.it>
2024-02-07 17:32:16 +0100

    componente Header

---
+++ src/components/CompHeader.vue
@@ -0,0 +1,61 @@
+<script>
+export default{
+    name: "CompHeader"
+}
+</script>
+
+
+<!-- ----------------------------------------------------------------------------- -->
+
+
+
+<template>
+
+    <header>
+        <figure>
+            <img src="../assets/img/dc-logo.png" alt="">
+        </figure>
+
+        <div>
+            <ul>
+                <li><a href="#"></a>character</li>
+                <li><a href="#"></a>comics</li>
+                <li><a href="#"></a>movies</li>
+                <li><a href="#"></a>tv</li>
+                <li><a href="#"></a>games</li>
+                <li><a href="#"></a>collectibles</li>
+                <li><a href="#"></a>videos</li>
+                <li><a href="#"></a>fans</li>
+                <li><a href="#"></a>news</li>
+                <li><a href="#"></a>shop</li>
+            </ul>
+        </div>
+    </header>
+    <main>
+        <p>--&gt; Content goes here &lt;--</p>
+    </main>
+
+
+</template>
+
+
+<!-- ----------------------------------------------------------------------------- -->
+
+
+<style lang="scss" scoped>
+
+header{
+    background-color: white;
+    display: flex;
+    ul{
+        li{
+            color: red;
+        }
+    }
+}
+p{
+    background-color: #1c1c1c;
+    padding: 50px;
+    color: white;
+}
+</style>
